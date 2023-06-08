3990f1b0ea443d62534a236c447d2dc72a360b10
Haitham abu lamdi <hayhtam14@gmail.com>
2023-06-08 15:49:14 +0300

    Project setup create Project setup, install needed dependencies and config eslint Relates #6

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: 'standard-with-typescript',
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json'],
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'linebreak-style': 0,
+    'consistent-return': 'off',
+  },
+  ignorePatterns: ['__test__/*', 'dist/*'],
+};
