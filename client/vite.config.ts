fb59fd55498d9aaa2943999b67df09303c7a2013
MohammedSallout <mzsalloot@gmail.com>
2023-06-10 16:16:47 +0300

    edit proxy from /api to /api/v1 Relates #7

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': {
+      '/api/v1': {
         target: 'http://localhost:4000',
         changeOrigin: true,
       }
