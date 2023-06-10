5b7c3e65434495c1aaae30cf44acde396762cc03
Haitham abu lamdi <hayhtam14@gmail.com>
2023-06-10 14:50:03 +0300

    Build server file structure change postbuild to deployment-postbuild and organize app.ts file as requested.

--- src/app.ts
+++ src/app.ts
@@ -2,15 +2,32 @@ import express from 'express'
 import compression from 'compression'
 import cookieParser from 'cookie-parser'
 import dotenv from 'dotenv'
+import { join } from 'path'
 import router from './routes'
 
 dotenv.config()
+const { NODE_ENV, PORT } = process.env
 const app = express()
+
 app.disabled('x-powered-by')
-app.set('port', process.env.PORT ?? 3000)
+
 app.use(express.json())
+
 app.use(express.urlencoded({ extended: false }))
+
 app.use(compression())
+
 app.use(cookieParser())
-app.use(router)
+
+app.use('/api/v1', router)
+
+if (NODE_ENV === 'production') {
+  app.use(express.static(join(__dirname, '..', 'client', 'build')))
+  app.get('/*', (_req, res) => {
+    res.sendFile(join(__dirname, '..', 'client', 'build', 'index.html'))
+  })
+}
+
+app.set('port', PORT ?? 3000)
+
 export default app
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import app from './app'
 app.listen(app.get('port'), (): void => {
-  console.log(`App live on: ${app.get('port') as number}`)
+  console.log(`App live on: http://localhost:${app.get('port') as number}`)
 })
