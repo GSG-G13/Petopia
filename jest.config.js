24e1441cd49cebdce5a6d62bb06c9bfbc6597813
MohammedSallout <mzsalloot@gmail.com>
2023-06-14 09:34:05 +0300

    edit jest.config.js Relates #17

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,1 @@
-module.exports = {
-  preset: 'ts-jest',
-  transform: {
-    '^.+\\.(ts|tsx)?$': 'ts-jest',
-    '^.+\\.(js|jsx)$': 'babel-jest',
-  },
-}
+module.exports = { preset: 'ts-jest', transform: { '^.+\\.(ts|tsx)?$': 'ts-jest', '^.+\\.(js|jsx)$': 'babel-jest', },}
