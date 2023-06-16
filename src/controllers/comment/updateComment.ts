845864d2802b3d41fb3e3c63971841c2fefe2796
MohammedSallout <mzsalloot@gmail.com>
2023-06-16 16:00:57 +0300

    handle update comment Relates #33

---
+++ src/controllers/comment/updateComment.ts
@@ -0,0 +1,28 @@
+import { type Request, type Response, type NextFunction } from 'express'
+import updateCommentQuery from '../../queries/comment/updateComment'
+import { type IComment } from '../../interfaces/models'
+import CustomError from '../../helpers/CustomError'
+import { commentSchema } from '../../validation/comment/addComment'
+
+const updateComment = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  try {
+    const { commentId } = req.params
+
+    const { commentText }: { commentText: string } = await commentSchema.validate(req.body, { abortEarly: false })
+
+    const updatedComment: IComment | null = await updateCommentQuery(Number(commentId), commentText)
+
+    if (updatedComment != null) {
+      res.json({
+        message: 'Comment Updated Successfully',
+        data: updatedComment
+      })
+    } else {
+      throw new CustomError(400, 'The Comment Was Not Found')
+    }
+  } catch (err: unknown) {
+    next(err)
+  }
+}
+
+export default updateComment
--- src/routes/comment.ts
+++ src/routes/comment.ts
@@ -3,10 +3,12 @@ import getComment from '../controllers/comment/getComment'
 import express from 'express'
 import addComment from '../controllers/comment/addComment'
 import deleteComment from '../controllers/comment/deleteComment'
+import updateComment from '../controllers/comment/updateComment'
 const commentRouter = express.Router()
 
 commentRouter.get('/post/:postId/comments', getComments)
 commentRouter.get('/comment/:commentId', getComment)
 commentRouter.post('/comment', addComment)
+commentRouter.put('/comment/:commentId', updateComment)
 commentRouter.delete('/comment/:commentId', deleteComment)
 export default commentRouter
