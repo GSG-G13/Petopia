fcc83c33ee7fc4611a8abc8b3e105d1fa1dae47f
AbdallahAbujazar <abdallah.abujazar@iths.se>
2023-06-18 02:01:13 +0300

    fix the code Relates #40

--- src/controllers/follow/index.ts
+++ src/controllers/follow/index.ts
@@ -1,7 +1,7 @@
 
 import { createFollow } from '../follow/add'
 import { unFollow } from '../follow/delete'
-import { showUserFollowing } from './UserFollowing'
-import { showUserFollowers } from './UserFollowers'
+import { showUserFollowing } from './userFollowing'
+import { showUserFollowers } from './userFollowers'
 
 export { createFollow, unFollow, showUserFollowing, showUserFollowers }
