3cdbc42cbe8bf014d0a264603fe69cbe77a856f0
MohammedSallout <mzsalloot@gmail.com>
2023-06-19 12:48:43 +0300

    handle limit comments Relates #46 Closes #46

--- src/controllers/comment/getComments.ts
+++ src/controllers/comment/getComments.ts
@@ -1,12 +1,22 @@
 import { type NextFunction, type Request, type Response } from 'express'
 import { getCommentsQuery } from '../../queries'
+import CustomError from '../../helpers/CustomError'
 
 const getComments = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
   const { postId } = req.params
   const id = Number(postId)
 
   try {
-    const comments = await getCommentsQuery(id)
+    const { page, limit } = req.query
+    const pageNumber = Number(page) || 1
+    const limitNumber = Number(limit) || 5
+    if (pageNumber < 0 || Number.isNaN(pageNumber) ||
+      limitNumber < 0 || Number.isNaN(limitNumber)) {
+      throw new CustomError(400, 'Bad Request.')
+    }
+    if (limitNumber >= 51) throw new CustomError(400, 'limit should not be more than 50')
+
+    const comments = await getCommentsQuery(id, pageNumber, limitNumber)
     res.json({
       message: 'Comments Retrieved Successfully',
       data: comments
