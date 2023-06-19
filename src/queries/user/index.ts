9196aefda8c665d5f1b31a3fb085084c38adae6d
Haitham abu lamdi <hayhtam14@gmail.com>
2023-06-19 15:54:08 +0300

    solve conflict

--- src/queries/user/index.ts
+++ src/queries/user/index.ts
@@ -1,9 +1,4 @@
 import getUserPostsQuery from './getUserPostsQuery'
 import getUserQuery from './getUserQuery'
-<<<<<<< HEAD
-
-export { getUserQuery, getUserPostsQuery }
-=======
 import getAllUsersQuery from './getAllUsersQuery'
-export { getUserQuery, getAllUsersQuery }
->>>>>>> 90da13e63b6b47f33a4742c54b1094b2788867c2
+export { getUserQuery, getAllUsersQuery, getUserPostsQuery }
