a04048b99a6dd7e6f0ba1ccb2fdf7a08d9411d4e
Muhammad Abdulhadi <mu7ammadabed@gmail.com>
2023-06-21 13:19:38 +0300

    add validation to the email of type email

--- src/controllers/user/updateUser.ts
+++ src/controllers/user/updateUser.ts
@@ -1,26 +1,26 @@
-import { Response, NextFunction } from 'express';
-import { CustomRequest } from '../../interfaces/iAuth';
-import { IUser } from '../../interfaces/fakeDataTypes';
-import { getUserQuery, editUserQuery } from '../../queries/user/';
-import bcrypt from 'bcrypt';
-import { validateEditUser } from '../../validation/user/';
-import CustomError from '../../helpers/CustomError';
+import { type Response, type NextFunction } from 'express'
+import { type CustomRequest } from '../../interfaces/iAuth'
+import { type IUser } from '../../interfaces/fakeDataTypes'
+import { getUserQuery, editUserQuery } from '../../queries/user/'
+import bcrypt from 'bcrypt'
+import { validateEditUser } from '../../validation/user/'
+import CustomError from '../../helpers/CustomError'
 
 interface UserData {
-  fullName: string;
-  email: string;
-  password: string;
-  userImage?: string;
-  profileImage?: string;
-  address?: string;
-  phone?: string;
+  fullName: string
+  email: string
+  password: string
+  userImage?: string
+  profileImage?: string
+  address?: string
+  phone?: string
 }
 
 const updateUser = async (req: CustomRequest, res: Response, next: NextFunction): Promise<void> => {
   try {
-    const userId: number | undefined = req.user?.userId;
+    const userId: number | undefined = req.user?.userId
     if (userId === undefined) {
-      throw new CustomError(401, 'User ID is missing or invalid');
+      throw new CustomError(401, 'User ID is missing or invalid')
     }
 
     const {
@@ -31,14 +31,14 @@ const updateUser = async (req: CustomRequest, res: Response, next: NextFunction)
       profileImage,
       address,
       phone
-    } = req.body as UserData;
+    } = req.body as UserData
 
     // Ensure that the user is updating their own information
     if (userId !== req.user?.userId) {
-      throw new CustomError(401, 'You are unauthorized to update this user');
+      throw new CustomError(401, 'You are unauthorized to update this user')
     }
 
-    const hashedPassword = await bcrypt.hash(password, 10);
+    const hashedPassword = await bcrypt.hash(password, 10)
 
     const userData: IUser = {
       fullName,
@@ -48,24 +48,24 @@ const updateUser = async (req: CustomRequest, res: Response, next: NextFunction)
       profileImage,
       address,
       phone
-    };
-
-    const user = await getUserQuery(Number(userId));
-    if (user === null) {
-      throw new CustomError(400, 'Bad Request');
     }
 
-    const updatedUser = await editUserQuery(userId, await validateEditUser.validate(userData));
+    const user = await getUserQuery(Number(userId))
+    if (user === null) {
+      throw new CustomError(400, 'Bad Request')
+    }
 
-    res.status(200).json({
+    const updatedUser = await editUserQuery(userId, await validateEditUser.validate(userData))
+
+    res.json({
       message: 'User Profile Updated Successfully',
       data: {
         user: updatedUser
       }
-    });
+    })
   } catch (err: unknown) {
-    next(err);
+    next(err)
   }
-};
+}
 
-export default updateUser;
+export default updateUser
