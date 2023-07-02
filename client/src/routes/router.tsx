1c32fd35071a5ef91f3d1a550ce82b2b24a21618
MohammedSallout <mzsalloot@gmail.com>
2023-07-02 17:01:43 +0300

    built side menu Relates #71

--- client/src/routes/router.tsx
+++ client/src/routes/router.tsx
@@ -4,6 +4,7 @@ import PageNotFound from '../components/commons/PageNotFound';
 import PostCard from '../components/post/PostCard';
 import fakeData from '../helpers/fakeData.json';
 import '../index.css';
+import SideBar from '../components/dashboard/Menu';
 
 const { post } = fakeData;
 const router = createBrowserRouter([
@@ -15,6 +16,10 @@ const router = createBrowserRouter([
   {
     path: '/post',
     element: <PostCard post={post} />,
+  },
+  {
+    path: '/dashboard',
+    element: <SideBar />,
   },
 ]);
 export default router;
