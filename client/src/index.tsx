2d7a96462e44e0be19e0ade53dd599fdd832f192
MohammedSallout <mzsalloot@gmail.com>
2023-07-02 19:09:05 +0300

    fix code Relates #52 Closes #52

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,26 +1,7 @@
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import SignUp from './pages/SignUp';
-import App from './App';
-import PostCard from './components/post/PostCard';
-import fakeData from './helpers/fakeData.json';
+import { RouterProvider } from 'react-router-dom';
 import './index.css';
-
-const { post } = fakeData;
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-  },
-  {
-    path: '/post',
-    element: <PostCard post={post} />,
-  },
-  {
-    path: 'signup',
-    element: <SignUp />,
-  },
-]);
+import router from './routes/router';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <RouterProvider router={router} />,
--- client/src/routes/router.tsx
+++ client/src/routes/router.tsx
@@ -4,6 +4,7 @@ import PageNotFound from '../components/commons/PageNotFound';
 import PostCard from '../components/post/PostCard';
 import fakeData from '../helpers/fakeData.json';
 import '../index.css';
+import SignUp from '../pages/SignUp';
 
 const { post } = fakeData;
 const router = createBrowserRouter([
@@ -11,6 +12,10 @@ const router = createBrowserRouter([
     path: '/',
     element: <App />,
     errorElement: <PageNotFound />,
+  },
+  {
+    path: '/signup',
+    element: <SignUp />,
   },
   {
     path: '/post',
